171c86d6687644d10613a8803e4a03784be55040
Okami <mr.naydyuk2@gmail.com>
2025-02-19 19:11:29 +0800

    API для получения всех пользователей

--- src/main/java/com/example/postgres/user/controller/UserApiController.java
+++ src/main/java/com/example/postgres/user/controller/UserApiController.java
@@ -7,7 +7,12 @@ import com.example.postgres.user.exception.UserNotFoundException;
 import com.example.postgres.user.repository.UserRepository;
 import com.example.postgres.user.routes.UserRoutes;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
@@ -39,5 +44,17 @@ public class UserApiController {
     public UserResponse byId(@PathVariable Long id) throws UserNotFoundException {
         return UserResponse.of(userRepository.findById(id).orElseThrow(UserNotFoundException::new));
 
+    }
+
+    @GetMapping(UserRoutes.SEARCH)
+    public List<UserResponse> search(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        return userRepository.findAll(pageable)
+                .stream()
+                .map(UserResponse::of)
+                .collect(Collectors.toList());
     }
 }
